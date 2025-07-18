88700d41e7769c7edd4582a7a4a434533045560b
Kauã Andrade <kauaa.matos@gmail.com>
2025-07-18 09:48:44 -0300

    commit inicial

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './style.css'
+import 'primeicons/primeicons.css'
+import router from './router'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
+
+const app = createApp(App);
+
+app.use(router);
+app.use(Toast);
+
+app.mount('#app');
